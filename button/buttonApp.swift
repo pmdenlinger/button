1255623178afd250eff696b96e1b5bbaeb0617a1
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-20 13:02:58 -0700

    Initial Commit

---
+++ button/buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttonApp.swift
+//  button
+//
+//  Created by user on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
